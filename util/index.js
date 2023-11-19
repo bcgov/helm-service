8ccde0411e4dbc88e7e3f27bb4f7a4f921553ac6
Om Mishra <32200996+mishraomp@users.noreply.github.com>
2023-11-18 19:57:29 -0800

    feat: Add unit test to PR workflow to make sure enhancements are backward compatible. (#27)

---
+++ util/index.js
@@ -0,0 +1,26 @@
+import * as fs from "fs";
+import jsYaml from "js-yaml";
+
+const fs_promises = fs.promises;
+const destination_repo = process.env.DESTINATION_REPO|| "quickstart-openshift";
+const sourceDirectory = ".";
+const main = async () => {
+  const chartYaml = await fs_promises.readFile("./helm-service/charts/component/Chart.yaml");
+  const destChartYaml = await fs_promises.readFile(`./${destination_repo}/charts/${destination_repo}/Chart.yaml`);
+  const chartYamlToJSON =jsYaml.load(chartYaml);
+  const version = chartYamlToJSON.version;
+  const destChartYamlToJSON =jsYaml.load(destChartYaml);
+  const dependencies = destChartYamlToJSON.dependencies;
+  let newDependencies = [];
+  for(const element of dependencies){
+    if(element.name === 'component'){
+      element.version = version;
+      element.repository = `file://../../../helm-service/charts/component`;
+    }
+    newDependencies.push(element);
+  }
+  destChartYamlToJSON.dependencies = newDependencies;
+  const newDestChartYaml = jsYaml.dump(destChartYamlToJSON);
+  await fs_promises.writeFile(`./${destination_repo}/charts/${destination_repo}/Chart.yaml`, newDestChartYaml);
+}
+await main();
